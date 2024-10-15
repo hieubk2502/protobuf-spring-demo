980ff504db30ca7d4997224cb72d626b9c1d6597
hieubk2502 <diemdungodau2502@gmail.com>
2024-10-15 21:09:27 +0700

    Init commit

---
+++ src/main/java/com/example/protobuf_spring/ProtobufSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.protobuf_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtobufSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtobufSpringDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/protobuf_spring/ProtobufSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.protobuf_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtobufSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
